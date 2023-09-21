eb04b63a7150e83b8355a99579148ff65129237c
zhang.yuan <zhang489@hotmail.com.br>
2023-09-21 15:43:37 -0300

    qwe

---
+++ unitest_spark/README.md
@@ -0,0 +1,4 @@
+ C:\Users\PC\Desktop\New folder\Unit_Test\unitest_spark>python -m unittest test.core_test   
+
+ python -m unittest test.core_test      
+                    <Folder> <file>    
---
+++ unitest_spark/test/core_test.py
@@ -0,0 +1,26 @@
+import unittest
+import logging
+from files.core import miles_to_km, km_to_miles
+
+# Get logger
+logger = logging.getLogger('__miles_to_km__')
+logger.setLevel(logging.INFO)
+
+class Test(unittest.TestCase):
+    def test_miles_to_km(self):
+        self.assertEqual(miles_to_km(1), 1.61)
+
+    def test_km_to_miles(self):
+        self.assertEqual(km_to_miles(1), 0.62)
+
+    def test_miles_to_km_dict_type(self):
+        with self.assertRaises(TypeError):
+            miles_to_km({'a': 1, 'b': 2})
+
+    def test_miles_to_km_list_type(self):
+        with self.assertRaises(TypeError):
+            miles_to_km([1,2,3])
+
+
+if __name__ == '__main__':
+    unittest.main()
